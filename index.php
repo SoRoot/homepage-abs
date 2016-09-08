a214cbf3a5c6f29d0786d12941233fe149d1463e
SoRoot <lukas.ungerland@gmail.com>
2016-09-08 18:45:38 +0200

    ajusting accordion on the grid

--- index.php
+++ index.php
@@ -2,235 +2,235 @@
 
 <html lang="de">
 
-<head>
+	<head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Akustik Bauphysik Schoenau">
-    <meta name="author" content="Lukas Ungerland">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Akustik Bauphysik Schoenau">
+		<meta name="author" content="Lukas Ungerland">
 
-    <title>akustik-sca</title>
+		<title>akustik-sca</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="style.css">
-     <!--link href="<!--?php bloginfo('style.css');? --> <! " rel="stylesheet" -->
+		<!-- CSS -->
+		<link rel="stylesheet" href="style.css">
+		 <!--link href="<!--?php bloginfo('style.css');? --> <! " rel="stylesheet" -->
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
 
-    <!--?php wp_enqueue_script("jquery"); ?-->
-    <!--?php wp_head(); ?-->
+		<!--?php wp_enqueue_script("jquery"); ?-->
+		<!--?php wp_head(); ?-->
 
-</head>
+	</head>
 
-<body id="page-top">
+	<body id="page-top">
 
-    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
-	<div class="container-fluid">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		<span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-		</button>
-		<a class="navbar-brand page-scroll" href="#page-top">AKUSTIK BAUPHYSIK SCHÖNAU</a>
-	    </div>
-
-	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		<ul class="nav navbar-nav navbar-right">
-		    <li>
-			<a class="page-scroll" href="#about">About</a>
-		    </li>
-		    <li>
-			<a class="page-scroll" href="#services">Services</a>
-		    </li>
-		    <li>
-			<a class="page-scroll" href="#portfolio">Portfolio</a>
-		    </li>
-		    <li>
-			<a class="page-scroll" href="#contact">Contact</a>
-		    </li>
-		</ul>
-	    </div>
-	    <!-- /.navbar-collapse -->
-	</div>
-	<!-- /.container-fluid -->
-    </nav>
-
-    <header>
-	<div class="header-content">
-	    <div class="header-content-inner">
-
-		<h2>25 Jahre Erfahrung</h2>
-		<h2>Beratung und Planung Akustik und Bauphysik</h2>
-		<hr>
-
-		<div class="row row-drop">
-			<div class="col-lg-8 col-lg-offset-2 text-center" id="accordion">
-				<div class="col-md-4">
-					<div class="accordionheader">
-						<p id="parag"><i class="fa fa-chevron-right" id="arrow" aria-hidden="true"></i> &nbsp;Schallschutz</p>
-					</div>
-					<div class="accordionbody">
-						<ul class="text-faded text-left center-points">
-							<li>Bauakustik</li>
-							<li>Schallimmissionsschutz</li>
-							<li>technische Akustik</li>
-						</ul>
-					</div>
+		<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+					</button>
+					<a class="navbar-brand page-scroll" href="#page-top">AKUSTIK BAUPHYSIK SCHÖNAU</a>
 				</div>
 
-				<div class="col-md-4">
-					<div class="accordionheader">
-						<p id="parag"><i class="fa fa-chevron-right" id="arrow" aria-hidden="true"></i> &nbsp;Akustische Messungen</p>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a class="page-scroll" href="#about">About</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#services">Services</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#portfolio">Portfolio</a>
+						</li>
+						<li>
+							<a class="page-scroll" href="#contact">Contact</a>
+						</li>
+					</ul>
+				</div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container-fluid -->
+		</nav>
+
+		<header>
+			<div class="header-content">
+				<div class="header-content-inner">
+
+					<h2>25 Jahre Erfahrung</h2>
+					<h2>Beratung und Planung Akustik und Bauphysik</h2>
+					<hr>
+
+					<div class="container">
+						<div class="row row-drop">
+							<div class="text-center" id="accordion">
+
+								<div class="col-md-4">
+									<div class="accordionheader">
+										<p id="parag"><i class="fa fa-chevron-right" id="arrow" aria-hidden="true"></i> &nbsp;Schallschutz</p>
+									</div>
+									<div class="accordionbody">
+										<ul class="text-faded text-left center-points">
+											<li>Bauakustik</li>
+											<li>Schallimmissionsschutz</li>
+											<li>technische Akustik</li>
+										</ul>
+									</div>
+								</div>
+
+								<div class="col-md-4">
+									<div class="accordionheader">
+										<p id="parag"><i class="fa fa-chevron-right" id="arrow" aria-hidden="true"></i> &nbsp;Akustische Messungen</p>
+									</div>
+									<div class="accordionbody">
+										<ul class="text-faded text-left center-points">
+											<li>Luft- und Trittschalldämmung</li>
+											<li>Schalldruckpegel</li>
+											<li>raumakustische Parameter</li>
+											<li>Messungen nach TA-Lärm</li>
+										</ul>
+									</div>
+								</div>
+
+								<div class="col-md-4">
+									<div class="accordionheader">
+										<p id="parag"><i class="fa fa-chevron-right" id="arrow" aria-hidden="true"></i> &nbsp;Thermische Bauakustik</p>
+									</div>
+									<div class="accordionbody">
+										<ul class="text-faded text-left center-points">
+											<li>Wärmeschutz</li>
+											<li>Energiebilanzierung</li>
+											<li>Feuchteschutz und Abdichtungstechnik</li>
+										</ul>
+									</div>
+								</div>
+
+							</div>
+						</div>
 					</div>
-					<div class="accordionbody">
+					<a href="#about" class="btn btn-primary btn-xl page-scroll">Erfahren Sie mehr</a>
+				</div>
+			</div>
+		</header>
+
+		<section class="bg-primary" id="about">
+			<div class="container">
+					<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 text-center">
+						<h2 class="section-heading">Was ich gelernt habe</h2>
+						<hr class="light">
 						<ul class="text-faded text-left center-points">
-							<li>Luft- und Trittschalldämmung</li>
-							<li>Schalldruckpegel</li>
-							<li>raumakustische Parameter</li>
-							<li>Messungen nach TA-Lärm</li>
+						<li>miteinander sprechen ist das A und O guten Gelingens</li>
+						<li>die Intuition offen zu halten für auch unkonventionelle Lösungen bringt meistens schöne Ergebnisse</li>
+						<li>Normen sind wichtig, aber nicht das Wichtigste</li>
 						</ul>
+						<a href="#services" class="page-scroll btn btn-default btn-xl">Get Started!</a>
+				</div>
+					</div>
+			</div>
+		</section>
+
+		<section id="services">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<h2 class="section-heading">Über mich</h2>
+						<hr class="primary">
 					</div>
 				</div>
-
-				<div class="col-md-4">
-					<div class="accordionheader">
-						<p id="parag"><i class="fa fa-chevron-right" id="arrow" aria-hidden="true"></i> &nbsp;Thermische Bauakustik</p>
+				<div class="row">
+					<div class="col-md-7 col-md-offset-1">
+						<div class="classification-box">
+							<p>Beratender Ingenieur</p>
+							<p>Bayerische Ingenieurekammer Bau</p>
+							<p>Nachweisberechtigter für Schall- Wärmeschutz Ingenieurkammer Hessen</p>
+							<p>Sachverständiger für Schall- und Wärmeschutz Ingenieurkammer-Bau Nordrhein-Westfalen</p>
+						</div>
+						<div class="shortcv-box">
+							<p>gelernter Bau- und Möbelschreiner</p>
+							<p>Dipl-Ing. (FH) Holztechnik</p>
+							<p>Fachhochschule Rosenheim</p>
+						</div>
 					</div>
-					<div class="accordionbody">
-						<ul class="text-faded text-left center-points">
-							<li>Wärmeschutz</li>
-							<li>Energiebilanzierung</li>
-							<li>Feuchteschutz und Abdichtungstechnik</li>
-						</ul>
+					<div class="col-md-3 colmdd-offset-1 ">
+						<img src="img/potrait.png" class="img-responsive" alt=""/>
 					</div>
 				</div>
 			</div>
+		</section>
 
-		</div>
-		<a href="#about" class="btn btn-primary btn-xl page-scroll">Erfahren Sie mehr</a>
-	    </div>
-	</div>
-    </header>
-
-    <section class="bg-primary" id="about">
-	<div class="container">
-	    <div class="row">
-		<div class="col-lg-8 col-lg-offset-2 text-center">
-		    <h2 class="section-heading">Was ich gelernt habe</h2>
-		    <hr class="light">
-		    <ul class="text-faded text-left center-points">
-		    <li>miteinander sprechen ist das A und O guten Gelingens</li>
-		    <li>die Intuition offen zu halten für auch unkonventionelle Lösungen bringt meistens schöne Ergebnisse</li>
-		    <li>Normen sind wichtig, aber nicht das Wichtigste</li>
-		    </ul>
-		    <a href="#services" class="page-scroll btn btn-default btn-xl">Get Started!</a>
-		</div>
-	    </div>
-	</div>
-    </section>
-
-    <section id="services">
-	<div class="container">
-	    <div class="row">
-		<div class="col-lg-12 text-center">
-		    <h2 class="section-heading">Über mich</h2>
-		    <hr class="primary">
-		</div>
-	    </div>
-	    <div class="row">
-		    <div class="col-md-7 col-md-offset-1">
-			    <div class="classification-box">
-				<p>Beratender Ingenieur</p>
-				<p>Bayerische Ingenieurekammer Bau</p>
-				<p>Nachweisberechtigter für Schall- Wärmeschutz Ingenieurkammer Hessen</p>
-				<p>Sachverständiger für Schall- und Wärmeschutz Ingenieurkammer-Bau Nordrhein-Westfalen</p>
-			    </div>
-				<div class="shortcv-box">
-				<p>gelernter Bau- und Möbelschreiner</p>
-				<p>Dipl-Ing. (FH) Holztechnik</p>
-				<p>Fachhochschule Rosenheim</p>
-			</div>
-		    </div>
-
-		    <div class="col-md-3 colmdd-offset-1 ">
-			<img src="img/potrait.png" class="img-responsive" alt=""/>
-		    </div>
-
-	    </div>
-	</div>
-    </section>
-
-    <section class="no-padding" id="portfolio">
-	<div class="container-fluid">
-		    <div class="row no-gutter popup-gallery">
-			<div class="col-md-12">
-			    <a class="portfolio-box" href="img/project1.png">
-				<img class="img-responsive" src="img/project1.png" alt="">
-				<div class="portfolio-box-caption">
-				    <div class="portfolio-box-caption-content">
-					<div class="project-category text-faded">
-						Neugestaltung Saal der Freiwilligen Feuerwehr Schönau
+		<section class="no-padding" id="portfolio">
+			<div class="container-fluid">
+				<div class="row no-gutter popup-gallery">
+					<div class="col-md-12">
+						<a class="portfolio-box" href="img/project1.png">
+						<img class="img-responsive" src="img/project1.png" alt="">
+						<div class="portfolio-box-caption">
+							<div class="portfolio-box-caption-content">
+								<div class="project-category text-faded">
+									Neugestaltung Saal der Freiwilligen Feuerwehr Schönau
+								</div>
+								<div class="project-name">
+									komplette Planung Raumakustik, Beleuchtung und Trockenbau
+								</div>
+							</div>
+						</div>
+						</a>
 					</div>
-					<div class="project-name">
-						komplette Planung Raumakustik, Beleuchtung und Trockenbau
-					</div>
-				    </div>
 				</div>
-			    </a>
 			</div>
-		    </div>
-	</div>
-    </section>
+		</section>
 
-    <section id="contact">
-	<div class="container">
-	    <div class="row">
-		<div class="col-lg-8 col-lg-offset-2 text-center">
-		    <h2 class="section-heading">Let's Get In Touch!</h2>
-		    <hr class="primary">
-		    <h2>AKUSTIK BAUPHYSIK SCHOENAU</h2>
-		    <p>Carl-Georg Ungerland</p>
-		    <p>Schwarzwaldstraße 5</p>
-		    <p>93464 Tiefenbach (Oberpfalz)</p>
-		</div>
-		<div class="col-lg-4 col-lg-offset-2 text-center">
-		    <i class="fa fa-phone fa-3x wow bounceIn"></i>
-		    <p>09673 9109849</p>
-		</div>
-		<div class="col-lg-4 text-center">
-		    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
-		    <p><a href="mailto:your-email@your-domain.com">c-g.ungerland@akustik-sca.de</a></p>
-		</div>
-	    </div>
-	</div>
-    </section>
+		<section id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2 text-center">
+						<h2 class="section-heading">Let's Get In Touch!</h2>
+						<hr class="primary">
+						<h2>AKUSTIK BAUPHYSIK SCHOENAU</h2>
+						<p>Carl-Georg Ungerland</p>
+						<p>Schwarzwaldstraße 5</p>
+						<p>93464 Tiefenbach (Oberpfalz)</p>
+					</div>
+					<div class="col-lg-4 col-lg-offset-2 text-center">
+						<i class="fa fa-phone fa-3x wow bounceIn"></i>
+						<p>09673 9109849</p>
+					</div>
+					<div class="col-lg-4 text-center">
+						<i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+						<p><a href="mailto:your-email@your-domain.com">c-g.ungerland@akustik-sca.de</a></p>
+					</div>
+				</div>
+			</div>
+		</section>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+		<!-- Bootstrap Core JavaScript -->
+		<script src="bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Plugin JavaScript -->
-    <script src="js/jquery.easing.min.js"></script>
-    <script src="js/jquery.fittext.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
-    <script src="js/wow.min.js"></script>
+		<!-- Plugin JavaScript -->
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/jquery.fittext.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+		<script src="js/wow.min.js"></script>
 
-    <!-- Custom Theme JavaScript -->
-    <script src="js/creative.js"></script>
+		<!-- Custom Theme JavaScript -->
+		<script src="js/creative.js"></script>
 
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 
-    <!--?php wp_footer(); ?-->
+		<!--?php wp_footer(); ?-->
 
-</body>
-
+	</body>
 </html>
