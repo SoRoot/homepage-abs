479941dbb46e2a84e0906435cb7815015d462e69
SoRoot <lukas.ungerland@gmail.com>
2016-10-12 16:40:46 +0200

    navbar centered

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		<title>Akustik-SCA</title>
 
 		<!-- CSS -->
-		<link rel="stylesheet" href="style.css">
+		<!--<link rel="stylesheet" href="style.css">-->
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
